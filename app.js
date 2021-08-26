e881a729c26fdc9ab76a9ed7beaece7ff8f65338
RaphaelHZ <83135772+RaphaelHZ@users.noreply.github.com>
2021-08-26 18:22:58 -0300

    mudança nos arquivos para testes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const qLogin = require('./queryLogin');
+const port = 5000;
+
+//proximas lihas para teste
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+//ate aqui
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Servidor do FixIn rodando na porta ${port}.`);
+});
+
+app.get('/login/', qLogin.getAllLogins);
+app.get('/login/:cpf', qLogin.getLoginCpf);
+app.post('/login/', qLogin.addLogin);
+app.put('/login/:cpf', qLogin.updateLogin);
+app.delete('/login/:cpf', qLogin.deleteLogin);
