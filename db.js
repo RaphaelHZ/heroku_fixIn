70b8367682128dce93d40200216b8dd4d27cc726
RaphaelHZ <83135772+RaphaelHZ@users.noreply.github.com>
2021-08-26 18:27:32 -0300

    direcionado para o db heroku

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 //Estava no do Hernando, vou tentar por para facilitar o uso nos módulos
-/* const Pool = require("pg").Pool;
+const Pool = require("pg").Pool;
 let connString = process.env.DATABASE_URL
 const pool = new Pool({
     connectionString : connString,
     port: 5432,
     ssl: { rejectUnauthorized: false }
 });
- */
 
-const Pool = require('pg').Pool;
+
+/* const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'admin',
   host: 'localhost',
   database: 'fixin',
   password: 'admin',
   port: 5432
-});
+}); */
 module.exports = pool;
