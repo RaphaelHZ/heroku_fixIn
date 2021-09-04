8101ae7c465a39fd1e1ee1afe696401a5b27d454
RaphaelHZ <83135772+RaphaelHZ@users.noreply.github.com>
2021-09-04 19:53:49 -0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const qLogin = require('./queryLogin');
 const qfiltro = require('./filtro');
 const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    app.use(cors());
+    next();
+});
 
 //proximas lihas para teste
 const bodyParser = require('body-parser')
